8091963702bbc897bc87334c6f99ad22d293b35d
xskull521 <higoranjos@hotmail.com>
2021-07-19 10:56:13 -0300

    Teste Moot It

---
+++ Higor/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Higor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btIncluir = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txCusto = new System.Windows.Forms.TextBox();
+            this.txDistancia = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbNao = new System.Windows.Forms.RadioButton();
+            this.rbSim = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txNivelDor = new System.Windows.Forms.TextBox();
+            this.btSalvar = new System.Windows.Forms.Button();
+            this.btCancelar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 50);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(810, 678);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btIncluir
+            // 
+            this.btIncluir.Location = new System.Drawing.Point(28, 8);
+            this.btIncluir.Name = "btIncluir";
+            this.btIncluir.Size = new System.Drawing.Size(153, 45);
+            this.btIncluir.TabIndex = 1;
+            this.btIncluir.Text = "Incluir";
+            this.btIncluir.UseVisualStyleBackColor = true;
+            this.btIncluir.Click += new System.EventHandler(this.btIncluir_Click);
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.Location = new System.Drawing.Point(256, 8);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(153, 45);
+            this.btExcluir.TabIndex = 2;
+            this.btExcluir.Text = "Excluir";
+            this.btExcluir.UseVisualStyleBackColor = true;
+            this.btExcluir.Click += new System.EventHandler(this.btExcluir_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btIncluir);
+            this.panel1.Controls.Add(this.btExcluir);
+            this.panel1.Location = new System.Drawing.Point(956, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 67);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btCancelar);
+            this.panel2.Controls.Add(this.btSalvar);
+            this.panel2.Controls.Add(this.txNivelDor);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.rbSim);
+            this.panel2.Controls.Add(this.rbNao);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.txDistancia);
+            this.panel2.Controls.Add(this.txCusto);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txData);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(961, 183);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(586, 459);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data";
+            // 
+            // txData
+            // 
+            this.txData.Location = new System.Drawing.Point(105, 33);
+            this.txData.Name = "txData";
+            this.txData.Size = new System.Drawing.Size(270, 20);
+            this.txData.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Custo";
+            // 
+            // txCusto
+            // 
+            this.txCusto.Location = new System.Drawing.Point(105, 88);
+            this.txCusto.Name = "txCusto";
+            this.txCusto.Size = new System.Drawing.Size(270, 20);
+            this.txCusto.TabIndex = 3;
+            // 
+            // txDistancia
+            // 
+            this.txDistancia.Location = new System.Drawing.Point(105, 145);
+            this.txDistancia.Name = "txDistancia";
+            this.txDistancia.Size = new System.Drawing.Size(270, 20);
+            this.txDistancia.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Distância";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Captura";
+            // 
+            // rbNao
+            // 
+            this.rbNao.AutoSize = true;
+            this.rbNao.Location = new System.Drawing.Point(121, 205);
+            this.rbNao.Name = "rbNao";
+            this.rbNao.Size = new System.Drawing.Size(45, 17);
+            this.rbNao.TabIndex = 7;
+            this.rbNao.Text = "Nao";
+            this.rbNao.UseVisualStyleBackColor = true;
+            // 
+            // rbSim
+            // 
+            this.rbSim.AutoSize = true;
+            this.rbSim.Location = new System.Drawing.Point(251, 205);
+            this.rbSim.Name = "rbSim";
+            this.rbSim.Size = new System.Drawing.Size(42, 17);
+            this.rbSim.TabIndex = 8;
+            this.rbSim.TabStop = true;
+            this.rbSim.Text = "Sim";
+            this.rbSim.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 262);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Nível dor";
+            // 
+            // txNivelDor
+            // 
+            this.txNivelDor.Location = new System.Drawing.Point(105, 259);
+            this.txNivelDor.Name = "txNivelDor";
+            this.txNivelDor.Size = new System.Drawing.Size(270, 20);
+            this.txNivelDor.TabIndex = 10;
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Location = new System.Drawing.Point(51, 349);
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(153, 45);
+            this.btSalvar.TabIndex = 11;
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.UseVisualStyleBackColor = true;
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // btCancelar
+            // 
+            this.btCancelar.Location = new System.Drawing.Point(251, 349);
+            this.btCancelar.Name = "btCancelar";
+            this.btCancelar.Size = new System.Drawing.Size(153, 45);
+            this.btCancelar.TabIndex = 12;
+            this.btCancelar.Text = "Cancelar";
+            this.btCancelar.UseVisualStyleBackColor = true;
+            this.btCancelar.Click += new System.EventHandler(this.btCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1625, 781);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "ACME FLIGHT MANAGER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btIncluir;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btCancelar;
+        private System.Windows.Forms.Button btSalvar;
+        private System.Windows.Forms.TextBox txNivelDor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbSim;
+        private System.Windows.Forms.RadioButton rbNao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txDistancia;
+        private System.Windows.Forms.TextBox txCusto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txData;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Higor/Form1.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Higor
+{
+    public partial class Form1 : Form
+    {
+        private bool incluindo = false;
+        private int idVooEditado = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VerificarBanco();
+
+            CarregarDados();
+
+        }
+
+        private void VerificarBanco()
+        {
+            var connection = AbrirConexao();
+            var command = connection.CreateCommand();
+            command.CommandText = "CREATE TABLE IF NOT EXISTS TB_VOO (ID_VOO integer PRIMARY KEY AUTOINCREMENT, DATA_VOO datetime, CUSTO numeric(10,2), DISTANCIA integer, CAPTURA char(1), NIVEL_DOR integer)";
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void CarregarDados()
+        {
+            var connection = AbrirConexao();
+
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT ID_VOO, DATA_VOO, CAPTURA, NIVEL_DOR FROM TB_VOO";
+
+            var dataTable = new DataTable();
+
+            dataTable.Load(command.ExecuteReader());
+
+            dataGridView1.DataSource = dataTable;
+            dataGridView1.Columns[0].Visible = false;
+
+            dataGridView1.Columns[1].HeaderText = "Data";
+            dataGridView1.Columns[2].HeaderText = "Captura";
+            dataGridView1.Columns[3].HeaderText = "Nível de dor";
+        }
+
+        private void LimparCampos()
+        {
+            txCusto.Text = "";
+            txData.Text = "";
+            txDistancia.Text = "";
+            txNivelDor.Text = "";
+            rbNao.Checked = false;
+            rbSim.Checked = false;
+            btSalvar.Enabled = false;
+            btCancelar.Enabled = false;
+        }
+
+        private SqliteConnection AbrirConexao()
+        {
+            var connection = new SqliteConnection("Data Source=acme.sqlite.db");
+
+            connection.Open();
+
+            return connection;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var connection = AbrirConexao();
+                var command = connection.CreateCommand();
+
+                var id = dataGridView1.SelectedRows[0].Cells[0].Value;
+                idVooEditado = Convert.ToInt32(id);
+
+                command.CommandText = "SELECT * FROM TB_VOO WHERE ID_VOO = " + id;
+
+                var reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    txData.Text = reader["DATA_VOO"].ToString();
+                    txCusto.Text = reader["CUSTO"].ToString();
+                    txDistancia.Text = reader["DISTANCIA"].ToString();
+
+                    if (reader["CAPTURA"].ToString() == "N")
+                        rbNao.Checked = true;
+                    else
+                        rbSim.Checked = true;
+
+                    txNivelDor.Text = reader["NIVEL_DOR"].ToString();
+
+
+                    btSalvar.Enabled = true;
+                    btCancelar.Enabled = true;
+                }
+                connection.Close();
+            }
+        }
+
+        private void btExcluir_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var id = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+
+                var connection = AbrirConexao();
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = "DELETE FROM TB_VOO WHERE ID_VOO = " + id;
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+                CarregarDados();
+            }
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            incluindo = false;
+        }
+
+        private void btIncluir_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            incluindo = true;
+            btSalvar.Enabled = true;
+            btCancelar.Enabled = true;
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            var connection = AbrirConexao();
+            var command = connection.CreateCommand();
+
+            if(incluindo == true)
+            {
+                var captura = "S";
+
+                if (rbNao.Checked == true)
+                    captura = "N";
+
+                command.CommandText = "INSERT INTO TB_VOO (DATA_VOO, CUSTO, DISTANCIA, CAPTURA, NIVEL_DOR) VALUES ('" + txData.Text +
+                    "','" + txCusto.Text +
+                    "','" + txDistancia.Text +
+                    "','" + captura +
+                    "','" + txNivelDor.Text +
+                    "')";
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                var captura = "S";
+
+                if (rbNao.Checked == true)
+                    captura = "N";
+
+                command.CommandText = "UPDATE TB_VOO SET DATA_VOO = '" + txData.Text +
+                    "', CUSTO = '" + txCusto.Text +
+                    "', DISTANCIA = '" + txDistancia.Text +
+                    "', CAPTURA = '" + captura +
+                    "', NIVEL_DOR = '" + txNivelDor.Text +
+                    "' WHERE ID_VOO = " + idVooEditado;
+
+                command.ExecuteNonQuery();
+
+            }
+
+            connection.Close();
+
+            LimparCampos();
+            CarregarDados();
+        }
+    }
+}
